6e127d67f9ad994cd6017a2e249d877e159df599
Deni Bertovic <deni@denibertovic.com>
2016-02-17 00:01:28 +0100

    Adds error handling for db connections
    This is mostly because when using docker-compose and the
    official postgres docker image it takes a while for the db
    container to start initially and the app would fail to connect and
    just give up. This way we retry and handle it gracefully.
    
    It's worth noting that the db containers only takes a while to start up
    during the first run. Subsequent runs will be faster.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
@@ -6,27 +7,38 @@ import           Database.Persist.Postgresql (runSqlPool)
 import           Network.Wai.Handler.Warp    (run)
 import           System.Environment          (lookupEnv)
 
+import           Control.Concurrent          (threadDelay)
+import           Control.Exception           (SomeException, catch)
+import           Control.Exception.Extra     (retry)
+import           Control.Retry
+
 import           Dummy.Api.Api               (app)
 import           Dummy.Api.Config            (Config (..), Environment (..),
                                               defaultConfig, makePool,
                                               setLogger)
+
 import           Dummy.Api.Models            (doMigrations)
 
 
 main :: IO ()
 main = do
-    env  <- lookupSetting "ENV" Development
+    env  <- lookupSetting "ENV" Test
     port <- lookupSetting "PORT" 8000
     pool <- makePool env
     let cfg = defaultConfig { getPool = pool, getEnv = env }
         logger = setLogger env
-    runSqlPool doMigrations pool
+    retry 5 $ retryDbConnection $ runSqlPool doMigrations pool
     run port $ logger $ app cfg
-
 
 lookupSetting :: Read a => String -> a -> IO a
 lookupSetting env def = do
     p <- lookupEnv env
     return $ case p of Nothing -> def
                        Just a  -> read a
+retryDbConnection :: IO a -> IO a
+retryDbConnection action = catch action $ \(_ :: SomeException) -> do
+    -- TODO: Got to figure out what exact exception to catch
+    putStrLn "Failed to connect to db. Retrying..."
+    threadDelay 5000000
+    action
 
