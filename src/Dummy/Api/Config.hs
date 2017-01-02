7deffd215c91d60f337fb29d93e10e7187ad3098
Deni Bertovic <deni@denibertovic.com>
2017-01-03 00:19:30 +0100

    Formatting

--- src/Dummy/Api/Config.hs
+++ src/Dummy/Api/Config.hs
@@ -29,18 +29,18 @@ defaultConfig = Config
     }
 
 setLogger :: Environment -> Middleware
-setLogger Test = id
+setLogger Test        = id
 setLogger Development = logStdoutDev
-setLogger Production = logStdout
+setLogger Production  = logStdout
 
 makePool :: Environment -> IO ConnectionPool
 makePool Test = runNoLoggingT $ createPostgresqlPool (connStr Test) (envPool Test)
 makePool e = runStdoutLoggingT $ createPostgresqlPool (connStr e) (envPool e)
 
 envPool :: Environment -> Int
-envPool Test = 1
+envPool Test        = 1
 envPool Development = 1
-envPool Production = 8
+envPool Production  = 8
 
 connStr :: Environment -> ConnectionString
 connStr Test = "host=localhost dbname=postgres user=postgres password=postgres port=5432"
