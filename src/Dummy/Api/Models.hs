9c2d4e794e6045b8ce39a32f94538454902db9d5
Deni Bertovic <deni@denibertovic.com>
2017-03-14 16:19:17 +0100

    Fixes initial user passwords

--- src/Dummy/Api/Models.hs
+++ src/Dummy/Api/Models.hs
@@ -19,6 +19,7 @@ import           Data.Aeson                  (FromJSON, ToJSON,
                                               (.:), (.:?), (.=))
 import qualified Data.Aeson                  as JSON
 
+import           Crypto.PasswordStore        (makePassword)
 import qualified Data.ByteString             as BS
 import           Data.Text                   as T
 import           Data.Text.Encoding          (decodeUtf8, encodeUtf8)
@@ -103,8 +104,10 @@ instance ToJSON Card
 instance FromJSON Card
 
 insertInitialUsers = do
-    Right jk <- insertBy $ User "John Doe" (Email "john@example.com") "password"
-    Right jak <- insertBy $ User "Jane Doe" (Email "jane@example.com") "password"
+    janePassword <- liftIO $ flip makePassword 17 $ encodeUtf8 "password"
+    johnPassword <- liftIO $ flip makePassword 17 $ encodeUtf8 "password"
+    Right jk <- insertBy $ User "John Doe" (Email "john@example.com") (decodeUtf8 johnPassword)
+    Right jak <- insertBy $ User "Jane Doe" (Email "jane@example.com") (decodeUtf8 janePassword)
     return (jk,  jak)
 
 inserInitialBoard iid = do
